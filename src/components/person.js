05b8d8f677bdb937e5195d7e9084c38319a9b392
SONIA-MANA <61407431+SONIA-MANA@users.noreply.github.com>
2020-03-06 15:08:54 +0300

    your awesome message

--- src/components/person.js
+++ src/components/person.js
@@ -3,6 +3,9 @@ import React from 'react'
 function person(person) {
     return (
         <div>
+          <ul>
+            {personList}
+          </ul>
           <h2>
             i am {person.name}. i am {person.age} years old. i know {person.skill}
           </h2>
